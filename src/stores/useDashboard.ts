ffbd1b625755f4fa3bdd435517e378535682cdf0
Xiong <xixiongchen@thestratos.org>
2024-03-20 17:10:35 -0400

    Make stratos-chain as the default value

--- src/stores/useDashboard.ts
+++ src/stores/useDashboard.ts
@@ -261,7 +261,7 @@ export const useDashboard = defineStore('dashboard', {
   state: () => {
     const favMap = JSON.parse(
       localStorage.getItem('favoriteMap') ||
-        '{"cosmos":true, "osmosis":true}'
+        '{"stratos":true,"cosmos":true, "osmosis":true}'
     );
     return {
       status: LoadingStatus.Empty,
